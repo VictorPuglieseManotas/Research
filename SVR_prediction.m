2f1098892bfc161c4f52012f0e28326f6932b6b0
Victor J Pugliese Manotas <victor.pugliese@ttu.edu>
2018-10-04 15:36:05 -0500

    Delete SVR_prediction.m

--- SVR_prediction.m
+++
@@ -1,9 +0,0 @@
-function Fr = SVR_prediction(SV,alpha,b,Cos,Sin,R,Eo)
-x=[Cos Sin R Eo];
-[m,~]=size(SV);
-aux=zeros(1,m);
-for i=1:m
-    aux(i)=exp(-1/length(x)*(norm(SV(i,:)-x)).^2);
-end
-Fr=b+sum(alpha.*aux');
-end
